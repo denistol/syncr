ba9391f76e7f352a047ca11977797379b3f20fb7
denistol <denistol@pm.me>
2025-12-24 04:43:51 +0200

    TEMP: tests disable

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,5 @@
 use std::{
-    env::{self, home_dir},
+    env::{self, current_dir, home_dir},
     os,
     path::PathBuf,
 };
@@ -9,16 +9,10 @@ use rsync::client::client::Client;
 
 #[test]
 fn test_client() {
-    dotenv().ok();
-    println!("ROOT_DIR: {:?}", env::var("ROOT_DIR"));
-    let p = PathBuf::from(env::var("ROOT_DIR").unwrap());
-    let c = Client::new(&p);
-    assert_eq!(true, true);
-}
-
-#[test]
-fn bit_test() {
-    let res = 5 & 2;
-    println!("{:x}", res);
+    // dotenv().ok();
+    // println!("ROOT_DIR: {:?}", env::var("ROOT_DIR"));
+    // let p = PathBuf::from(env::var("ROOT_DIR").unwrap());
+    // let p = PathBuf::from("value");
+    // let c = Client::new(&p);
     assert_eq!(true, true);
 }
